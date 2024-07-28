42b74e43e8322cd7f86573442002b02c254123fc
Agboola Olamidipupo <dipoagboola2019@gmail.com>
2024-07-29 00:32:35 +0100

    add box colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <main className="min-h-[100vh] flex flex-col items-center justify-center">
-      <div>send funds</div>
+      <div className="bg-white ">
+       
+      </div>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,61 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "fade-effect": {
+          "0%": {
+            transform: "scale(0.9)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "scale(1)",
+            opacity: "1",
+          },
+        },
+
+        "slide-up": {
+          "0%": {
+            transform: "translateY(100%)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+        },
+        "slide-down": {
+          "0%": {
+            transform: "translateY(-100%)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+        },
+        loader: {
+          "0%": {
+            opacity: "0.2",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        "fade-in": "fade-effect 300ms linear",
+        "slide-down": "slide-down 300ms linear forwards",
+        "slide-up": "slide-up 300ms linear forwards",
+        "rotate-clockwise": "rotate-clockwise 1s infinite linear",
+        "loader-opacity": "loader 1s ease-in-out alternate infinite",
+      },
+      colors: {
+        one: "0px 4px 4px -4px #0C0C0D0D",
+        two: "0px 16px 16px -8px #0C0C0D1A",
+      },
+    },
   },
   plugins: [],
-}
-
+};
