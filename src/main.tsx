a560fe1f55f395e159e8ff1682e71ee6780ce145
Agboola Olamidipupo <dipoagboola2019@gmail.com>
2024-07-29 00:08:52 +0100

    ψοοκινγ

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { HelmetProvider } from "react-helmet-async";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
+);
